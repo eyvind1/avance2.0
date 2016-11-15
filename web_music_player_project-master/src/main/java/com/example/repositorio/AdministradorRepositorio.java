9eae9544b5103bc690ea172b82470a710a75daeb
darkar5555 <rodrigo.castro@ucsp.edu.pe>
2016-11-14 19:56:39 -0500

    Segundo commit Mas repositorios
    creando el repositorio de administrados y persona pero en persona es
    clase abstracta

---
+++ web_music_player_project-master/src/main/java/com/example/repositorio/AdministradorRepositorio.java
@@ -0,0 +1,34 @@
+package com.example.repositorio;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+
+import com.example.dominio.Administrador;
+
+public interface AdministradorRepositorio extends Repository<Administrador, Integer> {
+
+	void delete(Administrador administrador);
+
+	Administrador save(Administrador administrador);
+
+
+	@Query("SELECT a FROM Administrador a WHERE a.id = :id")
+	Administrador buscarPorId(@Param("id") Integer id);
+
+	@Query("SELECT a FROM Administrador a")
+	List<Administrador> buscarTodos();
+
+	@Query("SELECT a FROM Administrador a where a.nombre = :nombre")
+	Administrador buscarPorNombre (@Param("nombre") String nombre);
+
+	@Query("SELECT a FROM Administrador a where a.apellidoPaterno = :apellidoPaterno")
+	Administrador buscarPorApellidoPaterno (@Param("apellidoPaterno") String apellidoPaterno);
+
+
+
+}
---
+++ web_music_player_project-master/src/main/java/com/example/repositorio/PersonaRepositorio.java
@@ -0,0 +1,1 @@
+PersonaRepositorio.java
