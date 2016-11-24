7cee06027c4536f4712c0289e245d26a2e24a3a9
EdherCerdan <josimar_93_12@hotmail.com>
2016-11-24 08:39:30 -0500

    Subo

--- web_music_player_project-master/src/main/java/com/example/repositorio/CancionRepositorio.java
+++ web_music_player_project-master/src/main/java/com/example/repositorio/CancionRepositorio.java
@@ -16,4 +16,6 @@ public interface CancionRepositorio extends Repository<Cancion, Integer> {
 
 	@Query("SELECT c FROM Cancion c ORDER BY c.Reproducciones DESC")
 	List<Cancion> Top100();
+	List<Cancion> findAll();
+	List<Cancion> findOne();
 }
