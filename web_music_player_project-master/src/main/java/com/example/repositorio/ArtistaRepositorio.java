30aae2a4550149eb14a2c47f07d52979a4734968
eyvind1 <eyvind.osco@ucsp.edu.pe>
2016-12-02 09:03:53 -0500

    subiendo

---
+++ web_music_player_project-master/src/main/java/com/example/repositorio/ArtistaRepositorio.java
@@ -0,0 +1,8 @@
+package com.example.repositorio;
+
+
+import com.example.dominio.Artista;
+import org.springframework.data.repository.Repository;
+
+public interface ArtistaRepositorio extends Repository<Artista,Long> {
+}
---
+++ web_music_player_project-master/src/main/java/com/example/repositorio/PersonaRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.repositorio;
+
+/**
+ * Created by eyvind on 2/12/16.
+ */
+public interface PersonaRepositorio {
+}
